ab38c1b47c3539f92cffffea51a35fa6ade08565
lordorito <maikolpedroza0@gmail.com>
2021-01-07 13:39:05 -0500

    se removieron los logs

--- js/get.js
+++ js/get.js
@@ -7,11 +7,10 @@ export async function getData(config){
 
     count = ( isMobile() && 5 ) || count
     apiKey = sortApiKeys(apiKeys)[0]
-    console.log(count)
 
     const url = `https://api.unsplash.com/photos/random?${query?'query='+query+'&':''}orientation=${orientation}&count=${count}&client_id=${apiKey}`
     const response = await fetch(url)
     const data     = await response.json()
-    
+
     return data
 }
